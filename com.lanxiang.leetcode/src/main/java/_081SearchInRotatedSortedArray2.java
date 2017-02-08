f41c4d5371c83f5d312dd609df0107a2b225401c
Lan Xiang <273109445@qq.com>
2017-02-08 23:50:02 +0800

    add leetcode module

---
+++ com.lanxiang.leetcode/src/main/java/_081SearchInRotatedSortedArray2.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+/**
+ * Created by lanjing on 2017/2/8.
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ *
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ *
+ * Write a function to determine if a given target is in the array.
+ *
+ * The array may contain duplicates.
+ *
+ * Subscribe to see which companies asked this question.
+ */
+
+public class _081SearchInRotatedSortedArray2 {
+
+    @Test
+    public void test(){
+
+    }
+}
