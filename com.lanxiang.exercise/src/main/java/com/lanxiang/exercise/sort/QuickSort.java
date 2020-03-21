7924fb6981a43ab8fffcbc795da7ad196b31eb33
兰兢 <lanjing@lanjings-MacBook-Pro.local>
2020-03-21 17:33:11 +0800

    update

--- com.lanxiang.exercise/src/main/java/com/lanxiang/exercise/sort/QuickSort.java
+++ com.lanxiang.exercise/src/main/java/com/lanxiang/exercise/sort/QuickSort.java
@@ -13,7 +13,7 @@ public class QuickSort {
         }
         int mid = divide(nums, low, high);
         sort(nums, low, mid - 1);
-        sort(nums, high, mid + 1);
+        sort(nums, mid + 1, high);
     }
 
     private int divide(int[] nums, int left, int right) {
@@ -34,7 +34,7 @@ public class QuickSort {
 
     @Test
     public void run() {
-//        int[] a = {5, 7, 3, 4, 6, 2, 1}
+//        int[] a = {5, 7, 3, 4, 6, 2, 1};
         int[] a = {1, 2, 3, 4, 5, 6};
         sort(a, 0, a.length - 1);
         for (int n : a) {
